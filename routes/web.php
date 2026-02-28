c3e395bb85b3e71e47bf54faf1b76600c308ead9
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-02-28 16:59:23 +0700

    lesson2: basic route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function (){
+    return 'Hello Laravel 12';
+});
+
+Route::get('/about', function (){
+    return 'About Us';
 });
